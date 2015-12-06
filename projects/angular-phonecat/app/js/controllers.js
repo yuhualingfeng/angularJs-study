a1703b53a16a846e828b0de34bae848116b5c4bd
yuhualingfeng <1135317965@qq.com>
2015-12-06 21:18:34 +0800

    step-4

--- projects/angular-phonecat/app/js/controllers.js
+++ projects/angular-phonecat/app/js/controllers.js
@@ -2,10 +2,11 @@ var phonecatApp = angular.module(['phonecatApp'],[]);
 
 phonecatApp.controller('PhoneListCtrl',function($scope){
 	$scope.phones =[
-	{'name':'Nexus S','snippet':'Fast Just go faster with Nexus S.'},
-	{'name':'Notorola XOOM with wi-Fi','snippet':'The Next,Net zGeneration tablet.'},
-	{'name':'Notorola XOOM','snippet':'The next,Next Genration tablet'}
+	{'name':'Nexus S','snippet':'Fast Just go faster with Nexus S.','age':1},
+	{'name':'Notorola XOOM\u2122 with wi-Fi','snippet':'The Next,Net zGeneration tablet.','age':2},
+	{'name':'Notorola XOOM\u2122','snippet':'The next,Next Genration tablet','age':3}
 	];
+	$scope.orderProp = 'age';
 
 	$scope.name = 'World'; 
 });
--- projects/angular-phonecat/test/unit/controllerSpec.js
+++ projects/angular-phonecat/test/unit/controllerSpec.js
@@ -8,14 +8,31 @@
 // 	});
 // });
 
+describe('PhoneCat controllers',function(){
+
+
 describe('PhoneListCtrl',function(){
 
+	var scope, ctrl;
 	beforeEach(module('phonecatApp'));
-	it('shoud create "phones" model width 3 phones',inject(function($controller){
-		var scope = {};
+
+	beforeEach(inject(function($controller){
+		scope = {};
 		ctrl = $controller('PhoneListCtrl',{$scope:scope});
-		expect(scope.phones.length).toBe(3);
-		expect(scope.name).toBe('World');
 	}));
 
+	it('shoud create "phones" model width 3 phones',function(){
+		expect(scope.phones.length).toBe(3);
+	});
+
+	it('should set the default value of orderProp to be age',function(){
+
+	  expect(scope.orderProp).toBe('age');
+
+	});
+
 });
+
+
+});
+
